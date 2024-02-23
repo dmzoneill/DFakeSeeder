9a621025a745d136885c52fd0cfaaf79db28efda
Automated Publisher <actions@users.noreply.github.com>
2024-02-23 23:16:38 +0000

    Automated publish: Fri Feb 23 23:16:38 UTC 2024 06dc613e46c1e94c89c38fccc71081f85476698a

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ entry_points = {
 
 setup_kwargs = {
     "name": "d-fake-seeder",
-    "version": "0.0.4",
+    "version": "0.0.5",
     "description": "",
     "long_description": "",
     "author": "David O Neill",
