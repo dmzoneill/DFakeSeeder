4e00538a1db715596fa530fcfcd7af3056fa69c7
Automated Publisher <actions@users.noreply.github.com>
2024-05-18 00:12:47 +0000

    Automated publish: Sat May 18 00:12:47 UTC 2024 4a981e353a1cc46ab2862d81f281b6861869439f

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ entry_points = {
 
 setup_kwargs = {
     "name": "d-fake-seeder",
-    "version": "0.0.36",
+    "version": "0.0.37",
     "description": "Python gtk4 fake torrent seeder",
     "long_description": "",
     "author": "David O Neill",
