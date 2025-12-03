4cc3c1c6968d8eefe7f61bdaabb3fdbf2382163f
Automated Publisher <actions@users.noreply.github.com>
2025-12-03 00:17:52 +0000

    Automated publish: Wed Dec  3 00:17:52 UTC 2025 60a195a10a0b54f9625716c658f94bd7a051de6a

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ class PostInstallCommand(install):
 
 setup_kwargs = {
     "name": "d-fake-seeder",
-    "version": "0.0.49",
+    "version": "0.0.50",
     "description": "BitTorrent seeding simulator for testing and development",
     "long_description": long_description,
     "long_description_content_type": "text/markdown",
